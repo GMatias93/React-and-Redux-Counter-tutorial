4a72779a085773adeec0a15d0bc4e43e397fc4a9
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-02-14 13:21:01 -0800

    feat: added action creators for for Incrementing and decrementing the counter

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-var store = Redux.createStore();
+// action types
+
+var INCREMENT = 'INCREMENT';
+var DECREMENT = 'DECREMENT';
+
+
+
+//action creators
+
+var incrementAction = function(){
+    return {
+        type: INCREMENT
+    };
+};
+
+var decrementAction = function(){
+    return {
+        type: DECREMENT
+    };
+};
